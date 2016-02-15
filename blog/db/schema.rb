170fe54627bce1f4df2031a0f781a4d4962f36c8
madajee <madajee@gmail.com>
2016-02-15 15:45:00 -0500

    Second commit

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208003939) do
+ActiveRecord::Schema.define(version: 20160214194943) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
