c7d504cd205f6fe2af6f9cfa4c59e0692560aa3a
madajee <madajee@gmail.com>
2016-02-19 12:18:07 -0500

    Third Commit

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -29,6 +29,13 @@ end
   def show
     @article = Article.find(params[:id])
   end
+
+   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
  
 private
   def article_params
